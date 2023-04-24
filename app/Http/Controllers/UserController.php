7b3af94f517ac9162ae2c21ade9455613fec16c8
John Curry <fastpenguin91@gmail.com>
2023-04-24 11:44:15 -0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\View\View;
+// use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    /**
+     * Show the profile for a given user.
+     */
+    public function show(string $id): View
+    {
+        return view('user.profile', [
+            'user' => User::findOrFail($id)
+        ]);
+    }
+}
